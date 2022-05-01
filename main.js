efcaa9b741d27a6e688dbddb31abb0b4fb058030
kofemannight@yandex.ru <kofemannight@yandex.ru>
2022-05-01 19:42:47 +0300

    2

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+"use strict";
+
 console.log('Файл подключен')
 
-function popupOpen(){
-  document.addEventListener('click', (e)=>{
-    console.log(e.target)
-  })
-}
+// document.addEventListener('click', (event) => {
+//   if(event.target.dataset.popup){
+//     const popupLink = event.target.dataset.popup;
+//     const popupBlock = document.getElementById(popupLink);
+//     popupBlock.classList.toggle('open')
+// // ! Не забыть удалить потом
+//     console.log('Попап открывается')
+//   }
+//   if(event.target.dataset.popupclose){
+//     const popupLink = event.target.dataset.popupclose;
+//     const popupBlock = document.getElementById(popupLink);
+//     popupBlock.classList.toggle('open')
+//     // ! Не забыть удалить потом
+//     console.log('Попап закрывается')
+//   }
+// })
